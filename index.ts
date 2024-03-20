245f2e7e3ae5e84a553623b2f3e2eadefa489516
shezanusman <shezanusman7@gmail.com>
2024-03-21 02:47:19 +0500

    tweak the text and added console at the end

--- index.ts
+++ index.ts
@@ -4,8 +4,8 @@
 
 import inquirer from "inquirer";
 const answer = await inquirer.prompt([
-  { message: "Enter Your firts number", type: "number", name: "firtsNumber" },
-  { message: "Enter Your second number", type: "number", name: "secondNumber" },
+  { message: "Enter firts number", type: "number", name: "firtsNumber" },
+  { message: "Enter second number", type: "number", name: "secondNumber" },
   {
     message: "Select one of operators to perform action",
     type: "list",
@@ -27,3 +27,5 @@ if (answer.operator === "ADDITION"){
     console.log(answer.firtsNumber / answer.secondNumber);
 }
 else {"please select the correct number"};
+
+console.log('THE END');
